f8015c170be9cf3be757c6c88e24d5b660ff03f0
jerry.zhu <jerryzhu@abc360.com>
2016-10-28 15:03:01 +0800

    update 增加一人多机型情况

--- src/main/java/com/qa/data/visualization/controllers/MobileController.java
+++ src/main/java/com/qa/data/visualization/controllers/MobileController.java
@@ -68,7 +68,7 @@ public class MobileController {
     @ResponseBody
     public DatatablesResponse<AndroidModelCnt> getAndroidModelCnt(HttpServletRequest request) {
         String customSQL = "select model as model,count(model) as count from ( select * from ABC360_ANDROID_APP_DEVICE_TBL where time > (UNIX_TIMESTAMP(now())*1000 - 3600*24*30*1000)) a inner JOIN\n" +
-                "(select uid,max(time) as time from ABC360_ANDROID_APP_DEVICE_TBL where LENGTH(time)=13 and time > (UNIX_TIMESTAMP(now())*1000 - 3600*24*30*1000) group by uid) b\n" +
+                "(select uid,max(time) as time from ABC360_ANDROID_APP_DEVICE_TBL where LENGTH(time)=13 and time > (UNIX_TIMESTAMP(now())*1000 - 3600*24*30*1000) group by uid,model) b\n" +
                 "on a.time = b.time and a.uid = b.uid\n" +
                 "GROUP BY (model) order by count desc";
         DatatablesCriterias criterias = DatatablesCriterias.getFromRequest(request);

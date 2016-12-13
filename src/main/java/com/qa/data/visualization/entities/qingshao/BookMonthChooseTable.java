8f957f249a844d97bec77388b30a8ed17ab09850
jerry.zhu <jerryzhu@abc360.com>
2016-12-13 17:23:45 +0800

    update 教研图增加数据表格

---
+++ src/main/java/com/qa/data/visualization/entities/qingshao/BookMonthChooseTable.java
@@ -0,0 +1,34 @@
+package com.qa.data.visualization.entities.qingshao;
+
+/**
+ * Created by dykj on 2016/12/13.
+ */
+public class BookMonthChooseTable {
+    private String book;
+    private String ccnt;
+    private String scnt;
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public String getCcnt() {
+        return ccnt;
+    }
+
+    public void setCcnt(String ccnt) {
+        this.ccnt = ccnt;
+    }
+
+    public String getScnt() {
+        return scnt;
+    }
+
+    public void setScnt(String scnt) {
+        this.scnt = scnt;
+    }
+}
